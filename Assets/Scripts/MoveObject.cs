e1e455cc7eb7c0fa920cf81d028fa76ebd7faed2
Park Eunsu <qkrdmstn1258@naver.com>
2024-02-20 13:31:06 +0900

    Background 무한맵

--- Assets/Scripts/MoveObject.cs
+++ Assets/Scripts/MoveObject.cs
@@ -7,11 +7,15 @@ public class MoveObject : MonoBehaviour
 {
     [SerializeField] private float speed;
 
+    
     // Update is called once per frame
     void Update()
     {
         Vector3 curPos = transform.position;
         Vector3 nxtPos = Vector3.left * speed * Time.deltaTime;
         transform.position = curPos + nxtPos;
+
+        if (transform.position.x <= -31)
+            transform.position += new Vector3(60, 0, 0);
     }
 }
